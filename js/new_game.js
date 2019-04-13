ea51e7909ba07958945ebfa622f2287057f0bb4b
David Okao <davidokao26@gmail.com>
2019-04-12 22:30:33 -0400

    stuff

--- js/new_game.js
+++ js/new_game.js
@@ -1,12 +1,15 @@
-// var name;
-var diff;
+var name;
+var diff; 
+
 function createPlayer() {
-	// name = document.getElementById("playerName").value;
+	name = document.getElementById("playerName").value;
 	diff = document.getElementById("difficultySelection").value;
-	window.location='player_created.html';
+	document.location.href = 'player_created.html';
     }
+
+
 
 function playerCreated(){
 	document.getElementById("diff").innerHTML = diff;
-	// document.getElementById("name").innerHTML = name;
+	document.getElementById("name").innerHTML = name;
 }
